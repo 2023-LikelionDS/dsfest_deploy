1a2b1cd64464db46044fa504fdecee466fa926ee
jiinkyung <jinkyung010310@gmail.com>
2023-05-15 16:44:36 +0900

    템플릿 url 변경

--- dsfest_BE/dsfest_BE/dsfest_BE/urls.py
+++ dsfest_BE/dsfest_BE/dsfest_BE/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from festapp import urls
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('festapp.urls')),
     path('', include('event.urls')),
-    path('', include('reviews.urls'))
+    path('', include('reviews.urls')),
+    re_path('', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
