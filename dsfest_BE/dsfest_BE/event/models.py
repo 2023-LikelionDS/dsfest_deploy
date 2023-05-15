982500cddc37428baa69fb96acbdb5c4e3011b95
jiinkyung <jinkyung010310@gmail.com>
2023-05-16 01:33:09 +0900

    낭만 내용 보이게

--- dsfest_BE/dsfest_BE/event/models.py
+++ dsfest_BE/dsfest_BE/event/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     author = models.CharField(max_length=10)
     content = models.CharField(max_length=500)
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
